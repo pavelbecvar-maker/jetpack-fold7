80f92017399af15d3730d2df430f53045ba52dc3
pavelbecvar-maker <pavel.becvar@gmail.com>
2026-08-27 13:24:49 +0200

    Refresh PWA cache for redesigned game

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jetpack-fold7-v1';
+const CACHE='jetpack-fold7-v2';
 const ASSETS=['./','index.html','game.js','manifest.webmanifest','icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
